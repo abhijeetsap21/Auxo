b4fb11cf006cf4ec3d57d5d525c3b21371a6e471
abhijeetsap21 <abbyastro@gmail.com>
2018-05-04 11:54:47 +0530

    Added try catch to New Employee Controller

--- Controllers/NewEmployeeController.cs
+++ Controllers/NewEmployeeController.cs
@@ -20,26 +20,44 @@ namespace NewLetter.Controllers
         // GET: NewEmployee
         public async Task<ActionResult> Index()
         {
-            Int64 companyID = Convert.ToInt64(BaseUtil.GetSessionValue(AdminInfo.companyID.ToString()));
-            var employerDetails = db.EmployerDetails.Where(e=>e.companyID== companyID);
-            return View(await employerDetails.ToListAsync());
+            try
+            {
+                Int64 companyID = Convert.ToInt64(BaseUtil.GetSessionValue(AdminInfo.companyID.ToString()));
+                var employerDetails = db.EmployerDetails.Where(e => e.companyID == companyID);
+                return View(await employerDetails.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = ex.Message.ToString();
+                BaseUtil.CaptureErrorValues(ex);
+                return RedirectToAction("Error");
+            }
         }
 
         // GET: NewEmployee/Details/5
         public async Task<ActionResult> Details(long? id)
         {
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                EmployerDetail employerDetail = await db.EmployerDetails.FindAsync(id);
+                if (employerDetail == null)
+                {
+                    return HttpNotFound();
+                }
+                var role_ = db.roles.Where(e => e.roleID == employerDetail.roleID).Select(e => new { e.role1 }).FirstOrDefault();
+                ViewBag.role = role_.role1.ToString();
+                return View(employerDetail);
             }
-            EmployerDetail employerDetail = await db.EmployerDetails.FindAsync(id);
-            if (employerDetail == null)
+            catch (Exception ex)
             {
-                return HttpNotFound();
+                TempData["msg"] = ex.Message.ToString();
+                BaseUtil.CaptureErrorValues(ex);
+                return RedirectToAction("Error");
             }
-            var role_= db.roles.Where(e => e.roleID == employerDetail.roleID).Select(e => new { e.role1 }).FirstOrDefault();
-            ViewBag.role = role_.role1.ToString();
-            return View(employerDetail);
         }
 
         // GET: NewEmployee/Create
@@ -56,46 +74,55 @@ namespace NewLetter.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create(EmployeeDetailValidation oEmployeeDetailValidation)
         {
-            EmployerDetail employerDetail = new EmployerDetail();
-            employerDetail.Name = oEmployeeDetailValidation.Name;
+            try
+            {
+                EmployerDetail employerDetail = new EmployerDetail();
+                employerDetail.Name = oEmployeeDetailValidation.Name;
                 employerDetail.Email = oEmployeeDetailValidation.Email;
-            employerDetail.Mobile = oEmployeeDetailValidation.Mobile;
-            employerDetail.isActive = false;
-            employerDetail.isDelete = false;
-            employerDetail.dataIsCreated = BaseUtil.GetCurrentDateTime();
-            employerDetail.dataIsUpdated = BaseUtil.GetCurrentDateTime();
-            Int64 roleid=Convert.ToInt64( BaseUtil.GetSessionValue(AdminInfo.role_id.ToString()));
-            employerDetail.password= baseClass.GetRandomPasswordString(10);
-            employerDetail.companyID= Convert.ToInt64(BaseUtil.GetSessionValue(AdminInfo.companyID.ToString()));
-            if (roleid == 2)
-            {
-                employerDetail.roleID = 3;
+                employerDetail.Mobile = oEmployeeDetailValidation.Mobile;
+                employerDetail.isActive = false;
+                employerDetail.isDelete = false;
+                employerDetail.dataIsCreated = BaseUtil.GetCurrentDateTime();
+                employerDetail.dataIsUpdated = BaseUtil.GetCurrentDateTime();
+                Int64 roleid = Convert.ToInt64(BaseUtil.GetSessionValue(AdminInfo.role_id.ToString()));
+                employerDetail.password = baseClass.GetRandomPasswordString(10);
+                employerDetail.companyID = Convert.ToInt64(BaseUtil.GetSessionValue(AdminInfo.companyID.ToString()));
+                if (roleid == 2)
+                {
+                    employerDetail.roleID = 3;
+                }
+                if (roleid == 1)
+                {
+                    employerDetail.roleID = 4;
+                }
+
+                if (ModelState.IsValid)
+                {
+                    db.EmployerDetails.Add(employerDetail);
+                    await db.SaveChangesAsync();
+                    var encryptedID = BaseUtil.encrypt(employerDetail.EmployerID.ToString());
+
+                    StreamReader sr = new StreamReader(Server.MapPath("/Emailer/toEmployerRegistrationSuccess.html"));
+
+                    string HTML_Body = sr.ReadToEnd();
+                    string newString = HTML_Body.Replace("#name", employerDetail.Name).Replace("#EMPID", encryptedID).Replace("#password", employerDetail.password);
+                    sr.Close();
+                    string To = employerDetail.Email.ToString();
+                    string mail_Subject = "Employer Registration Confirmation ";
+                    profileController objprofileController = new profileController();
+                    BaseUtil.sendEmailer(To, mail_Subject, newString, "");
+                    return RedirectToAction("Index");
+                }
+
+
+                return View(employerDetail);
             }
-            if (roleid ==1)
+            catch (Exception ex)
             {
-                employerDetail.roleID = 4;
+                TempData["msg"] = ex.Message.ToString();
+                BaseUtil.CaptureErrorValues(ex);
+                return RedirectToAction("Error");
             }
-
-            if (ModelState.IsValid)
-            {
-                db.EmployerDetails.Add(employerDetail);
-                await db.SaveChangesAsync();
-                var encryptedID = BaseUtil.encrypt(employerDetail.EmployerID.ToString());
-
-                StreamReader sr = new StreamReader(Server.MapPath("/Emailer/toEmployerRegistrationSuccess.html"));
-
-                string HTML_Body = sr.ReadToEnd();
-                string newString = HTML_Body.Replace("#name", employerDetail.Name).Replace("#EMPID", encryptedID).Replace("#password", employerDetail.password);
-                sr.Close();
-                string To = employerDetail.Email.ToString();
-                string mail_Subject = "Employer Registration Confirmation ";
-                profileController objprofileController = new profileController();
-                BaseUtil.sendEmailer(To, mail_Subject, newString, "");
-                return RedirectToAction("Index");
-            }
-
-          
-            return View(employerDetail);
         }
 
         // GET: NewEmployee/Edit/5
@@ -121,15 +148,24 @@ namespace NewLetter.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit(EmployerDetail employerDetail)
         {
-            employerDetail.dataIsUpdated = BaseUtil.GetCurrentDateTime();
-            if (ModelState.IsValid)
+            try
             {
-                db.Entry(employerDetail).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+                employerDetail.dataIsUpdated = BaseUtil.GetCurrentDateTime();
+                if (ModelState.IsValid)
+                {
+                    db.Entry(employerDetail).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+
+                return View(employerDetail);
             }
-           
-            return View(employerDetail);
+            catch (Exception ex)
+            {
+                TempData["msg"] = ex.Message.ToString();
+                BaseUtil.CaptureErrorValues(ex);
+                return RedirectToAction("Error");
+            }
         }
 
        
