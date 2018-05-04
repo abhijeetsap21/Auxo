987947cffa3a7ff44b3a6fe431855dbf716b7145
yogesh771 <yogeshwargupta771@gmail.com>
2018-05-04 18:28:20 +0530

    synch

--- Models/baseModel.cs
+++ Models/baseModel.cs
@@ -332,9 +332,15 @@ namespace NewLetter.Models
             List<string> list = new List<string>() { "BASE", "JSON", "HOME", "ACCOUNT", "JOBDETAILS","EMPLOYER","PROFILE" };
             return list;
         }
-       public static List<string> accesiblePages = null;
+      
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filterContext"></param>
+        /// <returns></returns>
         public static bool CheckAuthentication(ActionExecutingContext filterContext)
         {
+            List<string> accesiblePages = null;
             bool result = false;
 
             string actionName = filterContext.ActionDescriptor.ActionName;
